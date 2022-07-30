10c1c3aef631dd7d73e8ab4b636c6674cbb30cf1
Vinícius Martins <viniciusmartinsg@hotmail.com>
2022-07-29 23:15:00 -0300

    create(register): create register page.

---
+++ instagram/src/app/components/payloads/register.user.payload.ts
@@ -0,0 +1,6 @@
+export interface RegisterUserPayload {
+  email: string;
+  name: string;
+  username: string;
+  password: string;
+}
--- instagram/src/app/pages/login/login.page.ts
+++ instagram/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginUserPayload } from '../../components/payloads/login.user.payload';
+import { RegisterUserPayload } from '../../components/payloads/register.user.payload';
 import { HelperService } from '../../components/services/helper.service';
 
 @Component({
@@ -18,12 +19,28 @@ export class LoginPage implements OnInit {
   public user: LoginUserPayload = {
     user: '',
     password: '',
-  }
+  };
 
   public mockedUser: LoginUserPayload = {
     user: 'Vini',
     password: '123456'
   };
+
+  public mockedCreateUser: RegisterUserPayload = {
+    email: 'vini@email.com',
+    name: 'Vini',
+    username: 'vini',
+    password: '123456',
+  };
+
+  public createUser: RegisterUserPayload = {
+    email: '',
+    name: '',
+    username: '',
+    password: '',
+  };
+
+  public isNewUser: boolean = false;
 
   ngOnInit() {
   }
@@ -37,12 +54,44 @@ export class LoginPage implements OnInit {
     await this.router.navigate(['/home']);
   }
 
+  public async register(): Promise<void> {
+    if (!this.canRegister()) return;
+
+    if (!this.isEqualToMocked()) return;
+
+    await this.router.navigate(['/home']);
+  }
+
+  public isEqualToMocked(): boolean {
+    if (this.createUser.name !== this.mockedCreateUser.name) return false;
+
+    if (this.createUser.email !== this.mockedCreateUser.email) return false;
+
+    if (this.createUser.username !== this.mockedCreateUser.username) return false;
+
+    if (this.createUser.password !== this.mockedCreateUser.password) return false;
+
+    return true;
+  }
+
   public canLogin(): boolean {
     if (this.user.user.length < 4)
       return false;
 
     if (this.user.password.length < 6)
       return false;
+
+    return true;
+  }
+
+  public canRegister(): boolean {
+    if (this.createUser.email.length < 6) return false;
+
+    if (this.createUser.name.length < 4) return false;
+
+    if (this.createUser.username.length < 6) return false;
+
+    if (this.createUser.password.length < 6) return false;
 
     return true;
   }
